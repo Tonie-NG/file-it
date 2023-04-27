581879a9e471324077eaca4a1118b4628c566744
Tonie <victornwanochi@gmail.com>
2023-04-27 17:20:30 +0100

    feat: worked on home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,15 @@
 import LandingPage from "../../components/landing-page/LandingPage";
+import "./home.css";
 
 function Home() {
   return (
-    <div>
-      <LandingPage />
-    </div>
+    <>
+      {/* <LandingPage /> */}
+      <div className="homepage__container">
+        <button>Create</button>
+        <div className="home__files">bdvgwcfgh Lorem500</div>
+      </div>
+    </>
   );
 }
 
